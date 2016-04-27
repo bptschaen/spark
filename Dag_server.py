fceeb41a83fb0f57f53a6373722562a6d65c39e1
Brendan Tschaen <bptschaen@gmail.com>
2016-04-27 10:10:11 -0400

    listen to external ip's

--- Dag_server.py
+++ Dag_server.py
@@ -2,15 +2,13 @@ from flask import Flask
 from flask import request
 from flask import abort
 import urllib
+import json
 
 app = Flask(__name__)
 
 
 """
-example url: /newjob/0/%24RDD+%22MapPartitionsRDD%22+%281%29+StorageLevel%3A+StorageLevel%28false%2C+false%2C+false%2C+false%2C+1%29%3B+CachedPartitions%3A+0%3B+TotalPartitions%3A+10%3B+MemorySize%3A+0.0+B%3B+ExternalBlockStoreSize%3A+0.0+B%3B+DiskSize%3A+0.0+B%23%23%23RDD+%22ParallelCollectionRDD%22+%280%29+StorageLevel%3A+StorageLevel%28false%2C+false%2C+false%2C+false%2C+1%29%3B+CachedPartitions%3A+0%3B+TotalPartitions%3A+10%3B+MemorySize%3A+0.0+B%3B+ExternalBlockStoreSize%3A+0.0+B%3B+DiskSize%3A+0.0+B
-
-url2: $id=1 name=MapPartitionsRDD pid=0,###id=0 name=ParallelCollectionRDD pid=
-
+EXAMPLE CURL CALL:
 curl -i http://localhost:5000/newjob/0/%24sid%3D0%23id%3D1+name%3DMapPartitionsRDD+pid%3D0%2C%23%23%23id%3D0+name%3DParallelCollectionRDD+pid%3D
 
 """
@@ -26,27 +24,21 @@ def newJob( jobid,  dag ):
     stages = dag.split("$")
     for i in range(len(stages)):
         stageParse( stages[i] )
-        
     printDags( stages )
     return "TODO: config suggestion for job " + str(jobid)
 
 def printDags( stages ):
-    for i,  stage in enumerate(stages):
-        print "Stage " , i
-        #for rdd in stage:
-            #print "RDD: " + str(rdd)
+    return
 
 def stageParse( stage ):
     stageDict = {}
     #stageDict['stageId'] = 
     if not "sid=" in stage:
         return stageDict
-    print "stages[i]: ",  stage
     stageDict['stageId'] =  int( stage.split("sid=", 1)[1].split("#",1)[0] )
     rdds = stage.split("#",1)[1].split("###")
-    print "split stage: ",  stage
     stageDict['RDDs'] = rddParse( rdds )
-    print "stageDict: ",  stageDict
+    print json.dumps( stageDict )
 
 def rddParse( rdds ):
     rddFullList = []
@@ -60,8 +52,7 @@ def rddParse( rdds ):
         rddDict['parentIds'] = rddList[2].split("=")[1]
         #TODO: add more info about rdd here if necessary
         rddFullList.append( rddDict )
-    print "RDDs: ",  rddFullList
-    return rddDict
+    return  rddFullList
 
 @app.route( '/jobcompletion/<int:jobid>/<int:runtime>',  methods=['POST', 'GET'] )
 def updateWarehouse( jobid,  runtime ):
@@ -71,4 +62,4 @@ def updateWarehouse( jobid,  runtime ):
     #TODO: actually update warehouse
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',  debug=True)
